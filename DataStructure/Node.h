24541079afc9d90375f566f8dfa6b9bafd676b7d
arade <arade_tariku@yahoo.com>
2014-05-31 18:44:28 -0700

    Node created

--- DataStructure/Node.h
+++ DataStructure/Node.h
@@ -1,14 +1,19 @@
 #include "stdafx.h"
+#ifndef _Node_H
+#define _Node_H
 
-template <class T>
+template<class T>
 class Node{
 private: 
 	T value;
 	Node<T>* nextPtr;
 public:
-	Node(T const &value){};
+	Node(const T &value):value(value)nextPtr(nullptr){};
 	~Node(){};
-
-
-
+	void setValue(const T &val){ value = val; }
+	void setNextPtr(const Node<T>* ptr){ nextPtr = ptr; }
+	T getValue()const{return &value};
+	Node<T>* getNextPtr()const { return nextPtr; }
 };
+
+#endif
